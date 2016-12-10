bf77ad3782db1c03711951f405a0cd9af1c14b95
will.munn <willmunn@hotmail.com>
2016-12-10 18:06:28 +0000

    WIP use new serializer rather than writing xml as we go

--- unit-tests/serialization-tests.js
+++ unit-tests/serialization-tests.js
@@ -1,19 +1,25 @@
 const test = require('tape');
 const xmlbuilder = require('xmlbuilder');
 const xml2js = require('xml2js');
+const serialize = require('../lib/serialize');
 
 [
     {
         name: 'Single assert',
         input: {
             extra: [],
-            asserts: [ { ok: true, id: 3, name: 'should be equal' } ]
+            asserts: [ { ok: true, id: 3, name: 'should be equal' } ],
+            testName: 'hello test'
         },
         expected: {
-            '$': { tests: '1', failures: '0', skipped: '0' },
-            testcase: [
-                { '$': { name: '#3 should be equal' } }
-            ]
+            testsuites: {
+                testsuite: [{
+                    '$': { name: 'hello test', tests: '1', failures: '0', errors: '0' },
+                    testcase: [
+                        { '$': { name: '#3 should be equal' } }
+                    ]
+                }]
+            }
         }
     },
     {
@@ -23,13 +29,17 @@ const xml2js = require('xml2js');
             asserts: [ { skip: true, id: 3, ok: 'true', name: 'should be equal' } ]
         },
         expected: {
-            '$': { tests: '1', failures: '0', skipped: '1' },
-            testcase: [
-                {
-                    '$': { name: '#3 should be equal' },
-                    skipped: ['']
-                }
-            ]
+            testsuites: {
+                testsuite: [{
+                    '$': { tests: '1', failures: '0', skipped: '1', name: '', errors: '0' },
+                    testcase: [
+                        {
+                            '$': { name: '#3 should be equal' },
+                            skipped: ['']
+                        }
+                    ]
+                }]
+            }
         }
     },
     {
@@ -39,13 +49,17 @@ const xml2js = require('xml2js');
             asserts: [ { skip: false, id: 3, ok: false, name: 'should be equal' } ]
         },
         expected: {
-            '$': { tests: '1', failures: '1', skipped: '0' },
-            testcase: [
-                {
-                    '$': { name: '#3 should be equal' },
-                    failure: ['']
-                }
-            ]
+            testsuites: {
+                testsuite: [{
+                    '$': { tests: '1', failures: '1', name: '', errors: '0' },
+                    testcase: [
+                        {
+                            '$': { name: '#3 should be equal' },
+                            failure: ['']
+                        }
+                    ]
+                }]
+            }
         }
     },
     {
@@ -61,13 +75,18 @@ const xml2js = require('xml2js');
             } ]
         },
         expected: {
-            '$': { tests: '1', failures: '1', skipped: '0' },
-            testcase: [
-                {
-                    '$': { name: '#3 should be equal' },
-                    failure: ['\n          ---\n            operator: fail\n            at: Test\n          ...\n      ']
-                }
-            ]
+            testsuites: {
+                testsuite: [{
+                    '$': { tests: '1', failures: '1', name: '', errors: '0' },
+                    testcase: [
+                        {
+                            '$': { name: '#3 should be equal' },
+                            failure: [
+                                '\n          ---\n            operator: fail\n            at: Test\n          ...\n      ']
+                        }
+                    ]
+                }]
+            }
         }
     },
     {
@@ -80,23 +99,23 @@ const xml2js = require('xml2js');
           ]
         },
         expected: {
-            '$': { tests: '2', failures: '0', skipped: '0' },
-            testcase: [
-                { '$': { name: '#1 should be equal' } },
-                { '$': { name: '#2 should be equal' } },
-            ],
-            sysout: [ 'see me\n', 'me too\n' ]
+            testsuites: {
+                testsuite: [{
+                    '$': { tests: '2', failures: '0', name: '', errors: '0' },
+                    testcase: [
+                        { '$': { name: '#1 should be equal' } },
+                        { '$': { name: '#2 should be equal' } },
+                    ],
+                    sysout: [ 'see me\n', 'me too\n' ]
+                }]
+            }
         }
     }
 ].forEach(testCase => {
     test('serializes: ' + testCase.name, assert => {
         var asserts = [testCase.input];
         var xml = xml2js.parseString(serialize(asserts), (err, parsed) => {
-            var expected = {
-                testsuites: {
-                    testsuite: [testCase.expected]
-                }
-            };
+            var expected = testCase.expected;
             console.log('EXPECTED');
             console.dir(expected, {depth: null});
             console.log('ACTUAL');
@@ -106,50 +125,3 @@ const xml2js = require('xml2js');
         });
     });
 });
-
-function serialize (testCases) {
-  var rootXml = xmlbuilder.create('testsuites');
-  testCases.forEach(suite => {
-      const suiteElement = rootXml.ele('testsuite');
-      suiteElement.att('tests', suite.asserts.length);
-      suiteElement.att('skipped', suite.asserts.filter(a => a.skip).length);
-      suiteElement.att('failures', suite.asserts.filter(a => !a.ok).length);
-      suite.asserts.forEach(a => {
-        const testCaseElement = suiteElement.ele('testcase', {
-            name: '#' + a.id + ' ' + a.name
-        });
-        if(a.skip) {
-            testCaseElement.ele('skipped');
-        }
-        if(!a.ok) {
-            const failureElement = testCaseElement.ele('failure');
-            if(a.diag) {
-              failureElement.txt(formatFailure(a.diag));
-            }
-        }
-      });
-      suite.extra.forEach(e => {
-        suiteElement.ele('sysout', e);
-      });
-  });
-  return rootXml.end({
-    pretty: true,
-    indent: '  ',
-    newline: '\n'
-  });
-}
-
-function formatFailure(diag) {
-  var text = '\n          ---\n';
-
-  for(var key in diag) {
-    if(diag.hasOwnProperty(key) && diag[key] !== undefined) {
-      var value = diag[key];
-      text += '            '+key+': ' + (typeof value === 'object' ? JSON.stringify(value) : value) + '\n';
-    }
-  }
-
-  text += '          ...\n      ';
-
-  return text;
-}
