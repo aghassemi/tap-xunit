138294c8dd999ddfffc96dffd1f42de2b43f8134
Will Munn <will.munn@bp.com>
2016-12-30 13:12:02 +0000

    Dont strip line feeds

--- unit-tests/xml-sanitization-tests.js
+++ unit-tests/xml-sanitization-tests.js
@@ -3,9 +3,7 @@ var sanitizeString = require('../lib/sanitize-string');
 
 [
     '\u0000',
-    '\u0001',
-    '\uFEFF',
-    '\uFFFF',
+    '\u0001'
 ].forEach(function(invalidCharacter){
     test(
         'it strips the invalid character:' + invalidCharacter.charCodeAt(0),
@@ -16,3 +14,14 @@ var sanitizeString = require('../lib/sanitize-string');
     );
 });
 
+[
+    '\u000A',
+].forEach(function(validCharacter){
+    test(
+        'it doesn\'t strip the valid character:' + validCharacter.charCodeAt(0),
+        function(assert){
+            assert.equal(sanitizeString(validCharacter), validCharacter);
+            assert.end();
+        }
+    );
+});
