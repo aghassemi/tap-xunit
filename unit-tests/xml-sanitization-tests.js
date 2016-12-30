57689c4f0cc7ef22f7f4909b5be5bed4f115004e
Will Munn <will.munn@bp.com>
2016-12-30 12:49:54 +0000

    Slightly less naive string sanitization

---
+++ unit-tests/xml-sanitization-tests.js
@@ -0,0 +1,18 @@
+var test = require('tape');
+var sanitizeString = require('../lib/sanitize-string');
+
+[
+    '\u0000',
+    '\u0001',
+    '\uFEFF',
+    '\uFFFF',
+].forEach(function(invalidCharacter){
+    test(
+        'it strips the invalid character:' + invalidCharacter.charCodeAt(0),
+        function(assert){
+            assert.equal(sanitizeString(invalidCharacter), '');
+            assert.end();
+        }
+    );
+});
+
